6f3b328885e23ef33cbd78d3b38f8af72188da13
orgho Das <mcguireon@gmail.com>
2025-12-12 14:54:34 +0600

    socials app commit

--- social/models.py
+++ social/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+
+class LearningCircle(models.Model):
+    name = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CircleMembership(models.Model):
+    circle = models.ForeignKey(LearningCircle, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    joined_at = models.DateTimeField(auto_now_add=True)
+
+class CircleMessage(models.Model):
+    circle = models.ForeignKey(LearningCircle, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
